eb961469d3e0c5b9a88e1a5b36da9d3658a502ee
Robert Thiel <thielbert7@gmail.com>
2019-09-13 13:35:30 -0600

    Created a script with Coroutine that sets up my ability to spawn new enemies every 3 seconds (for now - may adjust later)... next, need to actually get them spawning in the correct place and control how towers choose which enemy to shoot at (nearest enemy).

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+	[SerializeField] float secondsBetweenSpawns = 3f;
+	[SerializeField] EnemyMovement enemy;
+
+	void Start() {
+		StartCoroutine(SpawnEnemies());
+	}
+
+	IEnumerator SpawnEnemies() {
+		while (true) {
+			print("Spawinging");
+			yield return new WaitForSeconds(secondsBetweenSpawns);
+		}
+	}
+}
