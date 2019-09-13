3f04a8c07da9e0a47ffbed4a341472c070a8557d
Robert Thiel <thielbert7@gmail.com>
2019-09-13 08:49:09 -0600

    Give enemy HP and when depleted, destroy gameObject (KillEnemy)

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour {
+	[SerializeField] int hitPoints = 40;
+
 	// Start is called before the first frame update
 	void Start() {
 	}
 
 	void OnParticleCollision(GameObject other) {
-		print("I'm hit!");
+		ProcessHit();
+		if (hitPoints < 1) {
+			KillEnemy();
+		}
+	}
+
+	void ProcessHit() {
+		hitPoints--;
+		print("Remaining HP: " + hitPoints);
+	}
+
+	void KillEnemy() {
+		Destroy(gameObject);
 	}
 }
