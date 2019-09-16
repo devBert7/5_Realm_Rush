e93108cdaab8dd631d5e013d58f3f0f2240e86c8
Robert Thiel <thielbert7@gmail.com>
2019-09-16 09:22:48 -0600

    Created box collider for enemies and friendly base so that OnTriggerEnter() in newly created PlayerHealth.cs script (when enemy collides with the base) - health decreases by 1 with a total of 3 HP available for now.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+	[SerializeField] int health = 3;
+
+	void OnTriggerEnter(Collider other) {
+		health--;
+		if (health <= 0) {
+			print("Game Over!");
+		}
+	}
+}
