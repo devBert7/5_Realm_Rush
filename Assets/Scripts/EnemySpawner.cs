db83a676f823724805141acf640bcc675d82f0b5
Robert Thiel <thielbert7@gmail.com>
2019-09-16 09:59:12 -0600

    Created UI Text for Enemies Spawned as well as Player Health so it's more appearant how many kills and when life is out.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour {
 	[Range(0.1f, 30f)][SerializeField] float secondsBetweenSpawns = 5f;
 	[SerializeField] EnemyMovement enemy;
 	[SerializeField] Transform parent;
+	[SerializeField] Text enemyText;
+	int numEnemies;
 
 	void Start() {
 		StartCoroutine(SpawnEnemies());
+		enemyText.text = numEnemies.ToString();
 	}
 
 	IEnumerator SpawnEnemies() {
 		while (true) {
+			numEnemies++;
+			enemyText.text = numEnemies.ToString();
 			var enemyClone = Instantiate(enemy, transform.position, Quaternion.identity);
 			enemyClone.transform.parent = parent;
 			yield return new WaitForSeconds(secondsBetweenSpawns);
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 	[SerializeField] int health = 3;
+	[SerializeField] Text healthText;
+
+	void Start() {
+		healthText.text = health.ToString();
+	}
 
 	void OnTriggerEnter(Collider other) {
 		health--;
+		healthText.text = health.ToString();
 		if (health <= 0) {
 			print("Game Over!");
 		}
